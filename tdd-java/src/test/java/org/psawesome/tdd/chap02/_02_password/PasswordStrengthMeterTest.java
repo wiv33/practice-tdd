a810d37a0139bf21177373ecb6263f2e4d5b1dcc
ps <wiv821253@gmail.com>
2020-07-17 22:06:58 +0900

    add step

--- tdd-java/src/test/java/org/psawesome/tdd/chap02/_02_password/PasswordStrengthMeterTest.java
+++ tdd-java/src/test/java/org/psawesome/tdd/chap02/_02_password/PasswordStrengthMeterTest.java
@@ -7,6 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * @author ps [https://github.com/wiv33/practice-tdd]
  * @role
+ *  길이사 8글자 이상,
+ *  0부터 9 사이의 숫자를 포함,
+ *  대문자 포함
+ *
+ *  세 규칙을 모두 충족하면 암호는 강함.
+ *  두 규칙을 충족하면 암호는 보통.
+ *  1개 이하의 규칙을 충족하면 암호는 약함.
  * @responsibility
  * @cooperate {
  * input:
@@ -19,7 +26,12 @@ public class PasswordStrengthMeterTest {
   @Test
   void meetsAllCriteria_Then_Strong() {
     var meter = new PasswordStrengthMeter();
+
     var result = meter.meter("ab12!@AB");
     assertEquals(PasswordStrength.STRONG, result);
+
+    var result2 = meter.meter("abc1!Add");
+    assertEquals(PasswordStrength.STRONG, result2);
+
   }
 }
