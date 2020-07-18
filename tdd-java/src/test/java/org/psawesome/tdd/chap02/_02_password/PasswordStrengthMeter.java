75777671f9d20e59ea483ec6fbaaadb93020969e
ps <wiv821253@gmail.com>
2020-07-18 18:44:33 +0900

    update last refactoring

--- tdd-java/src/test/java/org/psawesome/tdd/chap02/_02_password/PasswordStrengthMeter.java
+++ tdd-java/src/test/java/org/psawesome/tdd/chap02/_02_password/PasswordStrengthMeter.java
@@ -16,15 +16,20 @@ import java.util.Arrays;
 public class PasswordStrengthMeter {
   public PasswordStrength meter(String s) {
     if (s == null || s.isBlank()) return PasswordStrength.INVALID;
+    int metCounts = getMetCriteriaCounts(s);
+
+    if (metCounts <= 1) return PasswordStrength.WEAK;
+    if (metCounts == 2) return PasswordStrength.NORMAL;
+
+    return PasswordStrength.STRONG;
+  }
+
+  private int getMetCriteriaCounts(String s) {
     int metCounts = 0;
     if (s.length() >= 8) metCounts++;
     if (meetsContainingNumberCriteria(s)) metCounts++;
     if (meetsContainingUppercaseCriteria(s)) metCounts++;
-
-    if (metCounts == 1) return PasswordStrength.WEAK;
-    if (metCounts == 2) return PasswordStrength.NORMAL;
-
-    return PasswordStrength.STRONG;
+    return metCounts;
   }
 
   private boolean meetsContainingUppercaseCriteria(String s) {
