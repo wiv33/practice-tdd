8d7852064f51814a3cefc307e6dca1bbc7874c65
ps <wiv821253@gmail.com>
2020-07-19 02:36:49 +0900

    숫자를 포함했는지 테스트 추가

--- tdd-python/chap02/password_strength_meter.py
+++ tdd-python/chap02/password_strength_meter.py
@@ -6,4 +6,14 @@ class PasswordStrengthMeter:
     def meter(s):
         if len(s) < 8:
             return _PasswordStrength().NORMAL
+
+        contains_num = False
+        for ch in s:
+            if str.isdigit(ch):
+                contains_num = True
+                break
+
+        if not contains_num:
+            return _PasswordStrength().NORMAL
+
         return _PasswordStrength().STRONG
--- tdd-python/chap02/password_strength_meter_test.py
+++ tdd-python/chap02/password_strength_meter_test.py
@@ -7,6 +7,7 @@ class MyTestCase(unittest.TestCase):
     def test_constant(self):
         self.assertEqual(_PasswordStrength().STRONG, "STRONG")
 
+    # 첫 번째 테스트 - 전부 만족
     def test_meets_all_criteria_then_strong(self):
         meter = PasswordStrengthMeter()
         result = meter.meter("ab!@#AB2")
@@ -15,10 +16,19 @@ class MyTestCase(unittest.TestCase):
         result2 = meter.meter("abc12@3D")
         self.assertEqual(_PasswordStrength().STRONG, result2)
 
-    # 암호 조건 중 2개를 충족하므로 NORMAL
+    # 두 번째 테스트 - 암호 조건 중 2개를 충족하므로 NORMAL
     def test_meets_other_criteria_expect_for_length_then_normal(self):
         meter = PasswordStrengthMeter()
         result = meter.meter("ab12!@A")
+        self.assertEqual(_PasswordStrength().NORMAL, result)
+
+        result2 = meter.meter("Ab12!c")
+        self.assertEqual(_PasswordStrength().NORMAL, result2)
+
+    # 세 번째 테스트 - 숫자를 포함하지 않고 나머지 조건은 충족하는 경우
+    def test_meets_other_criteria_expect_for_number_then_normal(self):
+        meter = PasswordStrengthMeter()
+        result = meter.meter("ab!@ABqw")
         self.assertEqual(_PasswordStrength().NORMAL, result)
 
 
