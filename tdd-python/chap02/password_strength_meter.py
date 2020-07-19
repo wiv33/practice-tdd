aebf07e74d26c86d05b4503f9d0c6c3eb7964c0a
ps <wiv821253@gmail.com>
2020-07-19 09:56:49 +0900

    refactoring

--- tdd-python/chap02/password_strength_meter.py
+++ tdd-python/chap02/password_strength_meter.py
@@ -7,13 +7,18 @@ class PasswordStrengthMeter:
         if len(s) < 8:
             return _PasswordStrength().NORMAL
 
-        contains_num = False
-        for ch in s:
-            if str.isdigit(ch):
-                contains_num = True
-                break
+        contains_num = PasswordStrengthMeter.meets_containing_number_criteria(s)
 
         if not contains_num:
             return _PasswordStrength().NORMAL
 
         return _PasswordStrength().STRONG
+
+    @staticmethod
+    def meets_containing_number_criteria(s):
+        contains_num = False
+        for ch in s:
+            if str.isdigit(ch):
+                contains_num = True
+                break
+        return contains_num
